3052b731cd17900f88ce7a1087286b7b8b6f522d
Tester1313 <thiagohs1996@gmail.com>
2018-04-14 20:33:58 -0300

    ES6 - Definição de Let e Const

---
+++ es6/variavel.js
@@ -0,0 +1,13 @@
+var serie = 'Friends'
+
+//Constante que não pode ser alterada
+const serie3 = 'How i met your mother'
+
+if (true){
+//  let serie2 = 'Game of Thrones';
+    var serie2 = 'Game of Thrones';
+}
+
+//Variavel serie2 sofre elevação de escopo para tratar isso
+// a variavel deve ser declarada como let
+console.log(serie2);
