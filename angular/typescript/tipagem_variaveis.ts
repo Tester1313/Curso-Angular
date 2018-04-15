f05b1928c8b29789355b8facaa8eb7d679435647
Tester1313 <thiagohs1996@gmail.com>
2018-04-15 20:46:08 -0300

    Inferencia de Tipos typescript

---
+++ angular/typescript/tipagem_variaveis.ts
@@ -0,0 +1,12 @@
+let mensagem: string = "Seja bem-vindo"
+let tempodasFriends: number = 10
+let estudandoAngular:boolean = true
+
+//duas maneiras de declarar arrays
+let listaDeFrutas: Array<string> = ['uva','banana','melão']
+let listaDeFrutas2: string[] = ['uva','banana','melão']
+
+
+let notasDasProvas: Array<number> = [7.4,9,8]
+let notasDasProvas2: number[] = [7.4,9,8]
+
