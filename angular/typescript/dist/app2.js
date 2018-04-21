f0a24c9ec2060ec3443e099ac060c4298c01d149
Tester1313 <thiagohs1996@gmail.com>
2018-04-21 20:36:53 -0300

    Reliza a mdularização do código

--- angular/typescript/dist/app2.js
+++ angular/typescript/dist/app2.js
@@ -1,64 +1,22 @@
 "use strict";
-var Carro = /** @class */ (function () {
-    function Carro(modelo, numeroDePortas) {
-        this.velocidade = 0;
-        this.modelo = modelo;
-        this.numeroDePortas = numeroDePortas;
-    }
-    Carro.prototype.acelerar = function () {
-        this.velocidade = this.velocidade + 10;
-    };
-    Carro.prototype.parar = function () {
-        this.velocidade = 0;
-    };
-    Carro.prototype.velocidadeAtual = function () {
-        return this.velocidade;
-    };
-    return Carro;
-}());
-var Concessionaria = /** @class */ (function () {
-    function Concessionaria(endereco, listaDeCarros) {
-        this.endereco = endereco;
-        this.listaDeCarros = listaDeCarros;
-    }
-    Concessionaria.prototype.fornecerEndereco = function () {
-        return this.endereco;
-    };
-    Concessionaria.prototype.mostrarListaDeCarros = function () {
-        return this.listaDeCarros;
-    };
-    return Concessionaria;
-}());
-var Pessoa = /** @class */ (function () {
-    function Pessoa(nome, carroPreferido) {
-        this.nome = nome;
-        this.carroPreferido = carroPreferido;
-    }
-    Pessoa.prototype.dizerNome = function () {
-        return this.nome;
-    };
-    Pessoa.prototype.dizerCarroPreferido = function () {
-        return this.carroPreferido;
-    };
-    Pessoa.prototype.comprarCarro = function (carro) {
-        this.carro = carro;
-    };
-    Pessoa.prototype.dizerCarroQueTem = function () {
-        return this.carro;
-    };
-    return Pessoa;
-}());
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var Carro_1 = __importDefault(require("./Carro"));
+var Pessoa_1 = __importDefault(require("./Pessoa"));
+var Concessionaria_1 = __importDefault(require("./Concessionaria"));
 // criar carros
-var carroA = new Carro('dodge journey', 4);
-var carroB = new Carro('veloster', 3);
-var carroC = new Carro('cerato', 2);
+var carroA = new Carro_1.default('dodge journey', 4);
+var carroB = new Carro_1.default('veloster', 3);
+var carroC = new Carro_1.default('cerato', 2);
 // montar lista de carros concessionaria
 var listaDeCarros = [carroA, carroB, carroC];
-var concessionaria = new Concessionaria('Av Paulista', listaDeCarros);
+var concessionaria = new Concessionaria_1.default('Av Paulista', listaDeCarros);
 // exibir lista de carros
 //console.log(concessionaria.mostrarListaDeCarros());
 // Comprar o carro
-var cliente = new Pessoa('Thiago', 'veloster');
+var cliente = new Pessoa_1.default('Thiago', 'veloster');
 // Funcao map realiza loop em cada uma das posições do array
 concessionaria.mostrarListaDeCarros().map(function (carro) {
     if (carro['modelo'] == cliente.dizerCarroPreferido()) {
