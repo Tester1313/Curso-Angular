ba8aa0f08c964b5cfd21b3700cb55d42e732d485
Tester1313 <thiagohs1996@gmail.com>
2018-05-13 19:47:58 -0300

    Passa seletor como se fosse atributo de uma div

--- angular/projetos/app1/src/app/topo/topo.component.ts
+++ angular/projetos/app1/src/app/topo/topo.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-topo',
+    selector: '[app-topo]',
     templateUrl: './topo.component.html',
     // No caso do template pode-se utilizar '' ou "" ou `` onde o ultimo permite a quebra de linha
     //template: '<p>Esse é o componente topo</p>'
