4895825721a38cd0af9446a9ac3e2edfc64b790f
Tester1313 <thiagohs1996@gmail.com>
2018-05-13 19:35:38 -0300

    Corrige conflito de merge

--- angular/projetos/app1/src/app/topo/topo.component.ts
+++ angular/projetos/app1/src/app/topo/topo.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-topo',
     templateUrl: './topo.component.html'
+    // No caso do template pode-se utilizar '' ou "" ou `` onde o ultimo permite a quebra de linha
+    //template: '<p>Esse é o componente topo</p>'
 })
 export class TopoComponent {
-
 }
