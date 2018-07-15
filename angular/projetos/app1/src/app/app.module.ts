7a4391fb885d020bfbf59c0bd26e69b812788b3f
Tester1313 <thiagohs1996@gmail.com>
2018-07-15 19:24:39 -0300

    Cria components de progresso e tentativas
    Seção 3 Aula 44

--- angular/projetos/app1/src/app/app.module.ts
+++ angular/projetos/app1/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PainelComponent } from './painel/painel.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressoComponent } from './progresso/progresso.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
-    PainelComponent
+    PainelComponent,
+    TentativasComponent,
+    ProgressoComponent
   ],
   imports: [
     BrowserModule
