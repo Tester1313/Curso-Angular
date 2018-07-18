2d5a64cd759ca4c631740910f3be3fe88a657220
Tester1313 <thiagohs1996@gmail.com>
2018-07-18 20:06:41 -0300

    Cria model de frase
    Seção 5 Aula 54

---
+++ angular/projetos/app1/src/app/shared/frase.model.ts
@@ -0,0 +1,10 @@
+export class Frase {
+
+    fraseEng: string;
+    frasePtBr: string;
+
+    constructor(fraseEng: string, frasePtBr: string) {
+        this.fraseEng = fraseEng;
+        this.frasePtBr = frasePtBr;
+    }
+}
