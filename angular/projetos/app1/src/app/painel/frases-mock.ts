06f92df7198e3f4b15b634d10b3c15aa24dc67ff
Thiago Henrique <thiagohs1996@gmail.com>
2019-04-20 17:31:11 -0300

    Importa mock de frase na clase que será utilizada para teste

--- angular/projetos/app1/src/app/painel/frases-mock.ts
+++ angular/projetos/app1/src/app/painel/frases-mock.ts
@@ -1,6 +1,6 @@
-import { Frase } from '../shared/frase-model'
+import { Frase } from "../shared/frase.model";
 
-const FRASES: Frase[] = [
+export const FRASES: Frase[] = [
     { fraseEng: 'I like to learn' , frasePtBr: 'Eu gosto de aprender'},
     { fraseEng: 'I watch tv', frasePtBr: 'Eu assisto tv'},
     { fraseEng: 'How are you?', frasePtBr: 'Como vai você'},
