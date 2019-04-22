232f142f88c402d7cbfb220eaee3712d846e1e8d
Tester1313 <thiagohs1996@gmail.com>
2019-04-21 20:10:16 -0400

    Cria metodo para verificar resposta no click do botao

--- angular/projetos/app1/src/app/painel/painel.component.ts
+++ angular/projetos/app1/src/app/painel/painel.component.ts
@@ -23,6 +23,10 @@ export class PainelComponent implements OnInit {
 
   atualizaResposta(resposta: Event): void {
     this.resposta = (<HTMLInputElement>resposta.target).value;
-    console.log(this.resposta);
+    //console.log(this.resposta);
+  }
+
+  public verificarResposta(): void {
+    console.log('Verificar resposta: ', this.resposta);
   }
 }
